d833252da4a254b332a8b112eb358aced44775bd
Vivekpandey9113 <pandeyvivekanand817@gmail.com>
2026-05-30 18:43:32 +0530

    Added responsive mobile hamburger menu

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+
+  if (navLinks.classList.contains("active")) {
+        menuToggle.textContent = "✕";
+    } else {
+        menuToggle.textContent = "☰";
+    }
+});
+
+// Close menu after clicking a link
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+        menuToggle.textContent = "☰";
+    });
+});
