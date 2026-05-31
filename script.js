31f54a788d19a37aad36a13f5f279984786991ee
Vivekpandey9113 <pandeyvivekanand817@gmail.com>
2026-05-31 14:32:16 +0530

    Added WhatsApp button and appointment page

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Smooth Scroll Effect
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        const href = this.getAttribute('href');
+
+        if (href.startsWith('#')) {
+            e.preventDefault();
+
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
     });
 });
 
